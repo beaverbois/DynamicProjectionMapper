5abd200640c8abc06a8c53ea2c70a1ad22eae172
daniel <38931252+dgorbunov@users.noreply.github.com>
2024-11-10 06:44:37 -0500

    new camera

--- camera.py
+++ camera.py
@@ -1,5 +1,7 @@
 import cv2
 from consts import Consts
+import pyfreenect2
+import signal
 
 class Camera:
     def __init__(self):
@@ -13,3 +15,67 @@ class Camera:
     def getFrame(self):
         _, frame = self.cam.read()
         return frame
+
+class Kinect:
+    def __init__(self):
+        # This is pretty much a straight port of the Protonect program bundled with
+        # libfreenect2.
+
+        # Initialize device
+        serialNumber = pyfreenect2.getDefaultDeviceSerialNumber()
+        kinect = pyfreenect2.Freenect2Device(serialNumber)
+
+        # Set up signal handler
+        self.terminated = False
+
+        def sigint_handler(signum, frame):
+            print("Got SIGINT, shutting down...")
+            self.terminated = True
+            kinect.stop()
+
+        signal.signal(signal.SIGINT, sigint_handler)
+
+        # Set up frame listener
+        self.frameListener = pyfreenect2.SyncMultiFrameListener(pyfreenect2.Frame.COLOR,
+                                                        pyfreenect2.Frame.IR,
+                                                        pyfreenect2.Frame.DEPTH)
+
+        print(self.frameListener)
+        kinect.setColorFrameListener(self.frameListener)
+        kinect.setIrAndDepthFrameListener(self.frameListener)
+
+        # Start recording
+        kinect.start()
+
+        # Print useful info
+        print("Kinect serial: %s" % kinect.serial_number)
+        print("Kinect firmware: %s" % kinect.firmware_version)
+
+
+        self.registration = pyfreenect2.Registration(kinect)
+
+        # # Initialize OpenCV stuff
+        # cv2.namedWindow("RGB")
+        # cv2.namedWindow("Depth")
+
+    def getFrame(self):
+        if self.terminated:
+            return
+        frames = self.frameListener.waitForNewFrame()
+        rgbFrame = frames.getFrame(pyfreenect2.Frame.COLOR)
+        depthFrame = frames.getFrame(pyfreenect2.Frame.DEPTH)
+        (undistorted, registered, big) = self.registration.apply(rgbFrame=rgbFrame, depthFrame=depthFrame)
+
+        depth_frame = big.getDepthData()
+        rgb_frame = rgbFrame.getRGBData()
+
+        self.frameListener.release(frames)
+
+        return rgb_frame, depth_frame
+
+    
+if __name__ == "__main__":
+    kinect = Kinect()
+    for i in range(1000):
+        kinect.getFrame()
+        
